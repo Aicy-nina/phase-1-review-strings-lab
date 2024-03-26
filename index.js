90edb8b12b2cd3a4c480e59da7caae7930b4b76c
Aicy-nina <muhikanina@gmail.com>
2024-03-26 11:51:41 +0300

    review string lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+const welcomeMessage = `Welcome to Flatbook,${currentUser}!`;
+const exitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortgreeting = `Welcome, ${currentUser.slice(0,1)}!`;
